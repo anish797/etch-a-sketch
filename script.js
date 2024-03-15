c94c6eb139fe63edb7501565f5cb3d7a24aa85b8
anish797 <anishlol797@gmail.com>
2024-03-15 15:31:05 +0530

    changed from hover

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 function createGrid() {
   const grid = document.querySelector(".grid");
-  const btn = document.querySelector(".reset");
+  const resetBtn = document.querySelector(".reset");
+  const colorInput = document.querySelector("input[type='color']");
 
-  let dim = prompt("Enter dimension of grid");
-  let x = 640 / dim;
+  let isDrawing = false;
 
-  for (let i = 1; i <= dim; i++) {
-    const pixelRow = document.createElement("div");
-    for (let j = 1; j <= dim; j++) {
-      const pixel = document.createElement("div");
-      pixel.classList.add("pixel");
-      pixel.style.height = `${x}px`;
-      pixel.style.width = `${x}px`;
-      pixelRow.appendChild(pixel);
+  function createNewGrid() {
+    grid.innerHTML = "";
+    let dim = prompt("Enter dimension of grid");
+    let x = 400 / dim;
+
+    for (let i = 1; i <= dim; i++) {
+      const pixelRow = document.createElement("div");
+      for (let j = 1; j <= dim; j++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.height = `${x}px`;
+        pixel.style.width = `${x}px`;
+        pixelRow.appendChild(pixel);
+      }
+      pixelRow.classList.add("row");
+      grid.appendChild(pixelRow);
     }
-    pixelRow.classList.add("row");
-    grid.appendChild(pixelRow);
+
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+      pixel.addEventListener("mousedown", () => {
+        isDrawing = true;
+        setColor(pixel);
+      });
+
+      pixel.addEventListener("mouseenter", () => {
+        if (isDrawing) {
+          setColor(pixel);
+        }
+      });
+
+      pixel.addEventListener("mouseup", () => {
+        isDrawing = false;
+      });
+    });
   }
 
-  const pixels = document.querySelectorAll(".pixel");
-  pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = "blue";
-    });
-  });
+  function setColor(pixel) {
+    pixel.style.backgroundColor = colorInput.value;
+  }
 
-  btn.addEventListener("click", () => {
-    window.location.reload();
-  });
+  resetBtn.addEventListener("click", createNewGrid);
 }
 
 createGrid();
